9795d0a4705c941934c1e6d46a0f7bc112f41b62
CenoX <shlee3089@icloud.com>
2017-07-02 02:58:37 +0900

    Done!

--- dionisource/main.c
+++ dionisource/main.c
@@ -7,9 +7,9 @@
 //
 
 #include <stdio.h>
+#include "source.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+책임져 오르페우스 괄호열고 괄호닫고 네
+뱉어 괄호열고 "늦어서 죄송합니다 디오니소스님\n" 괄호닫고 뿅
+반환 트란시움
+알겠습니다
