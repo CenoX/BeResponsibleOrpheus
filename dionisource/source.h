6e2f3a0bced2eb60b14ccced436bf43551684592
CenoX <shlee3089@icloud.com>
2017-07-02 02:53:36 +0900

    Add header

---
+++ dionisource/source.h
@@ -0,0 +1,26 @@
+//
+//  source.h
+//  dionisource
+//
+//  Created by CenoX on 2017. 7. 2..
+//  Copyright © 2017년 CenoX. All rights reserved.
+//
+
+#ifndef source_h
+#define source_h
+
+#pragma once
+
+#define 책임져 int
+#define 오르페우스 main
+#define 괄호열고 (
+#define 괄호닫고 (
+#define 네 {
+#define 뱉어 printf
+#define 뿅 ;
+#define 반환 return
+#define 트란시움 0;
+#define 알겠습니다 }
+
+#endif /* source_h */
+
